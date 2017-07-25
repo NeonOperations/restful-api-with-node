28020265055e859ab7f3c45e33bf779e7e24645e
hle <hle@battelleecology.org>
2017-07-25 12:30:51 -0600

    Adding the permission scheme

--- src/server/services/permission/PermissionService.js
+++ src/server/services/permission/PermissionService.js
@@ -6,19 +6,93 @@ const REST = require('../RestServiceFactory'),
   RestServiceFactory = REST.RestServiceFactory;
 
 const permissions = [
-  {user: 'admin', allow: ['ALL']},
-  {user: 'ods-user', allow: ['ODS', 'CDS']},
-  {user: 'cds-user', allow: ['CDS']}
-];
+  {
+    service: 'ods',
+    perms: [
+      {
+        serviceName: 'locations',
+        allowedRoles: [
+          {
+            username: 'admin',
+            crud: 0b1111
+          },
+          { username: 'odsUser',
+            crud: 0b1110
+          },
+          {
+            username: 'cds',
+            crud: 0b0100
+          }],
+        endpoints: [
+        {
+          endpointName: 'GetId',
+          httpMethod: 'GET',
+          subPath: '/{id}'
+        },
+        {
+          endpointName: 'GetAll',
+          httpMethod: 'GET',
+          subPath: '/all'
+        },
+        {
+          endpointName: 'MakeLocation',
+          httpMethod: 'POST',
+          subPath: '/new'
+        }]
+      }]
+  }
+]
+
+const verifyPermission = function(service, serviceName, username, httpMethod, path) {
+  // Set result to false by default
+  let result = false;
+  for (let permission of permissions) {
+    if (permission['service'] === service) {
+      const servicePerms = permission['perms'];
+      for (let perm of servicePerms) {
+        if (perm['serviceName'] === serviceName) {
+          const allowedRoles = perm['allowedRoles'];
+          const endpoints = perm['endpoints'];
+          for (let endpoint of endpoints) {
+            if (endpoint['httpMethod'] === httpMethod && endpoint['subPath'] === path) {
+              for (let role of allowedRoles) {
+                if (role['username'] === username) {
+                  const permSettings = role['crud'];
+                  switch (httpMethod) {
+                    case 'POST':
+                      result = Boolean(permSettings & 0b1000);
+                      break;
+                    case 'GET':
+                      result = Boolean(permSettings & 0b0100);
+                      break;
+                    case 'PUT':
+                      result = Boolean(permSettings & 0b0010);
+                      break;
+                    case 'DELETE':
+                      result = Boolean(permSettings & 0b0001);
+                      break;
+                    default:
+                      break;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
 
 const requestPermission = (req, res) => {
-
-  if (!req.body) {
-    //res.sendStatus(400);
-  }
-
-  const isTokenValid = identity.ValidateToken(req, res);
-  console.log(isTokenValid);
+  const body = JSON.parse(req.body);
+  const ret = verifyPermission(body.service,
+    body.serviceName,
+    body.user,
+    body.method,
+    body.path);
+  res.json({success: true, payload: ret});
 }
 
 module.exports.service = new RestServiceFactory("Permission",
